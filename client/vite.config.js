6f25b1857bfc3633e008d2d1c05518d86731ee07
AzizurDRahaman <89570145+AzizurDRahaman@users.noreply.github.com>
2024-07-03 23:47:16 +0530

    Fixed a bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build:{
     rollupOptions:{
-      external:['react-toastify','react-router-dom','react-icons', 'react-toastify/dist/ReactToastify.css']
+      external:['react-toastify','react-toastify/dist/ReactToastify.css']
     }
   }
 })
